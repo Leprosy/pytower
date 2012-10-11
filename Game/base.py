62c417eda56f03624c4cc9026701f5a486ac1dd0
Miguel Rojas <mrojas@redmike.(none)>
2012-10-11 18:11:05 -0300

    Base done

--- Game/base.py
+++ Game/base.py
@@ -1,5 +1,6 @@
 import pyglet
 from pyglet.window import mouse
+from Game.actors import Tower, Creep
 
 
 class Game(pyglet.window.Window):
@@ -11,15 +12,45 @@ class Game(pyglet.window.Window):
         pyglet.resource.reindex()
 
         #Resources, actors and stuff
-        self.image = pyglet.resource.image('me.jpg')
+        self.Towers = list()  # Towers in the map
+        self.Creeps = list()  # The creeps
+        self.Towers.append(Tower('firebolt', 200, 300))
+        self.Creeps.append(Creep('treanth', 10, 500))
+
+        self.x = 0
+        self.y = 0
+        self.bgimage = pyglet.resource.image('bg1.png')
+
+    #Methods
+    def draw_ent(self, x, y, r=255, g=255, b=255):
+        pyglet.graphics.draw(6, pyglet.gl.GL_POLYGON,
+            ('v2i', (x - 10, y - 4,
+                     x, y - 12,
+                     x + 10, y - 4,
+                     x + 10, y + 4,
+                     x, y + 12,
+                     x - 10, y + 4)),
+             ('c3B', (
+                      r, g, b,
+                      r, g, b,
+                      r, g, b,
+                      r, g, b,
+                      r, g, b,
+                      r, g, b))
+        )
 
     #Events handlers
     def on_draw(self):
         self.clear()
-        self.image.blit(0, 0)
+        self.bgimage.blit(0, 0)
+        self.draw_ent(self.x, self.y, 200, 200, 240)
 
-    def on_mouse_press(self, x, y, button, modifiers):
-        print x, y, button, modifiers
+        for Tower in self.Towers:
+            self.draw_ent(Tower.x, Tower.y, 10, 10, 240)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.x = x
+        self.y = y
 
     #Starts
     def game_start(self):
