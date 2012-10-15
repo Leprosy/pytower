9852fc497b42c90307a4e8b4138822ac49780c2c
Miguel Rojas <mrojas@redmike>
2012-10-15 10:45:54 -0300

    Data files and mayor physics methods and other stuff

--- Game/actors.py
+++ Game/actors.py
@@ -1,4 +1,6 @@
-
+import pyglet
+import math
+import time
 
 class Entity:
     """
@@ -9,16 +11,121 @@ class Entity:
         self.name = name
         self.xp = 0
         self.level = 1
+
+        self.alive = True
+
         self.x = x
         self.y = y
+        self.radius = 5
 
-    def render(self, screen):
+        self.r = 255
+        self.g = 255
+        self.b = 255
+
+    def render(self):
+        pyglet.graphics.draw(6, pyglet.gl.GL_POLYGON,
+            ('v2f', (self.x - 10, self.y - 4,
+                     self.x, self.y - 12,
+                     self.x + 10, self.y - 4,
+                     self.x + 10, self.y + 4,
+                     self.x, self.y + 12,
+                     self.x - 10, self.y + 4)),
+             ('c3B', (
+                      self.r, self.g, self.b,
+                      self.r, self.g, self.b,
+                      self.r, self.g, self.b,
+                      self.r, self.g, self.b,
+                      self.r, self.g, self.b,
+                      self.r, self.g, self.b))
+        )
+
+    def dist(self, ent):
+        return math.sqrt(math.pow(self.x - ent.x, 2) +
+                math.pow(self.y - ent.y, 2))
+
+    def angle(self, ent):
+        return math.atan2(ent.y - self.y, ent.x - self.x)
+
+    def update(self):
         pass
 
 
 class Creep(Entity):
-    pass
+    def __init__(self, name, path, speed=1):
+        Entity.__init__(self, name, path[0].x, path[0].y)
+        self.r = 200
+        self.g = 40
+        self.b = 20
+        self.speed = speed
+
+        self.path = path
+        self.path_point = 1
+
+    def update(self):
+        #Update the position, in function of path point
+        angle = self.angle(self.path[self.path_point])
+        xdif = self.speed * math.cos(angle)
+        ydif = self.speed * math.sin(angle)
+
+        self.y = self.y + ydif
+        self.x = self.x + xdif
+
+        #Reach point?
+        if self.dist(self.target) < self.target.radius:
+            self.path_point += 1
+
+        #End of the line?
+        if self.path_point == len(self.path):
+            print "OUCH"
 
 
 class Tower(Entity):
-    pass
+    def __init__(self, name, x=0, y=0):
+        Entity.__init__(self, name, x, y)
+        self.r = 40
+        self.g = 40
+        self.b = 200
+
+        self.cooldown = 0.8
+        self.cooldown_timer = 0
+        self.bullet = None
+
+    def shoot(self, target):
+        if self.bullet is None:
+            if time.time() - self.cooldown_timer > self.cooldown:
+                self.cooldown_timer = time.time()
+                self.bullet = Bullet("bang", target, self.x, self.y)
+
+    def render(self):
+        Entity.render(self)
+
+        if self.bullet is not None and self.bullet.alive == False:
+            self.bullet = None
+
+        if self.bullet is not None:
+            self.bullet.render()
+            self.bullet.update()
+
+class Bullet(Entity):
+    def __init__(self, name, ent, x=0, y=0, speed=6):
+        Entity.__init__(self, name, x, y)
+        self.r = 40
+        self.g = 40
+        self.b = 0
+
+        self.speed = speed
+        self.target = ent
+
+    def update(self):
+        #Update the position, in function of target's position
+        angle = self.angle(self.target)
+        xdif = self.speed * math.cos(angle)
+        ydif = self.speed * math.sin(angle)
+
+        self.y = self.y + ydif
+        self.x = self.x + xdif
+
+        #Impact?        
+        if self.dist(self.target) < self.target.radius:
+            self.alive = False
+
--- Game/base.py
+++ Game/base.py
@@ -1,6 +1,7 @@
 import pyglet
+import json
 from pyglet.window import mouse
-from Game.actors import Tower, Creep
+from Game.actors import Tower, Creep, Entity
 
 
 class Game(pyglet.window.Window):
@@ -12,45 +13,54 @@ class Game(pyglet.window.Window):
         pyglet.resource.reindex()
 
         #Resources, actors and stuff
+        self.config = json.load(open('res/maps/map1/data.json'))
+
         self.Towers = list()  # Towers in the map
         self.Creeps = list()  # The creeps
+        self.AliveCreeps = list()
+        self.Cursor = Entity('mouse', 0, 0)
         self.Towers.append(Tower('firebolt', 200, 300))
-        self.Creeps.append(Creep('treanth', 10, 500))
+
+        for i in range(10):
+            self.Creeps.append(Creep('treanth', self.config.path, 1))
+        self.creep_cooldown = 3
+        self.creep_cooldowntimer = 0
 
         self.x = 0
         self.y = 0
         self.bgimage = pyglet.resource.image('bg1.png')
 
-    #Methods
-    def draw_ent(self, x, y, r=255, g=255, b=255):
-        pyglet.graphics.draw(6, pyglet.gl.GL_POLYGON,
-            ('v2i', (x - 10, y - 4,
-                     x, y - 12,
-                     x + 10, y - 4,
-                     x + 10, y + 4,
-                     x, y + 12,
-                     x - 10, y + 4)),
-             ('c3B', (
-                      r, g, b,
-                      r, g, b,
-                      r, g, b,
-                      r, g, b,
-                      r, g, b,
-                      r, g, b))
-        )
-
     #Events handlers
     def on_draw(self):
+        #Clear window
         self.clear()
-        self.bgimage.blit(0, 0)
-        self.draw_ent(self.x, self.y, 200, 200, 240)
 
+        #Backgrounds
+        self.bgimage.blit(0, 0)
+        self.Cursor.render()
+
+        #Towers
         for Tower in self.Towers:
-            self.draw_ent(Tower.x, Tower.y, 10, 10, 240)
+            Tower.render()
+
+            if Tower.dist(self.Cursor) < 200:
+                Tower.shoot(self.Cursor)
+
+        #Creeps
+        if len(self.AliveCreeps) < len(self.Creeps):
+            if time.time() - self.creep_cooldowntimer > self.creep_cooldown:
+                self.creep_cooldowntimer = time.time()
+                self.AliveCreeps.append(self.Creep[0])
+
+        for Creep in self.AliveCreeps:
+            Creep.update()
+            Creep.render()
+
+        #GUI
 
     def on_mouse_motion(self, x, y, dx, dy):
-        self.x = x
-        self.y = y
+        self.Cursor.x = x
+        self.Cursor.y = y
 
     #Starts
     def game_start(self):
